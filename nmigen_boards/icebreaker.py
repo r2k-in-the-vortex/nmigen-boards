55b181bce872eb9d3f92a5a95233f6d2674c062a
rainer kordmaa <->
2021-06-29 22:14:39 +0300

    maybe not exe after all

--- nmigen_boards/icebreaker.py
+++ nmigen_boards/icebreaker.py
@@ -63,7 +63,7 @@ class ICEBreakerPlatform(LatticeICE40Platform):
     ]
 
     def toolchain_program(self, products, name):
-        iceprog = os.environ.get("ICEPROG", "iceprog.exe")
+        iceprog = os.environ.get("ICEPROG", "iceprog")
         with products.extract("{}.bin".format(name)) as bitstream_filename:
             subprocess.check_call([iceprog, "-b", bitstream_filename])
 
